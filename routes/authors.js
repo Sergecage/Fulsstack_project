bbda2891fbe1ebf4647a708c2e2d247866e3abed
Sergecage <34719013+Sergecage@users.noreply.github.com>
2020-12-04 23:47:52 +0300

    Commit 5

--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const Author = require('../models/author')
+const Author = require('../models/authors')
 
 //all AUthors route
 router.get('/', (req, res) => {
@@ -13,8 +13,20 @@ router.get('/new', (req, res) => {
 })
 
 //Create Author Route
-router.post('/', (req, res) => {
-    res.send('Create')
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    try {
+        const newAuthor = await author.save()
+        // res.redirect('authors/${newAuthor.id}')
+            res.redirect('authors')
+    } catch {
+        res.render('authors/new', {
+            author:author,
+            errorMessage: "Error creating Author"
+        })
+    }
 })
 
 module.exports = router 
