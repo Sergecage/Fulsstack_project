17a688cdb711d4dccfc8224adb683524197730ad
Sergecage <34719013+Sergecage@users.noreply.github.com>
2020-12-06 00:10:02 +0300

    Initial Author Routes

--- routes/authors.js
+++ routes/authors.js
@@ -3,8 +3,19 @@ const router = express.Router()
 const Author = require('../models/authors')
 
 //all AUthors route
-router.get('/', (req, res) => {
-    res.render("authors/index")
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render("authors/index", {
+            authors:authors,
+        searchOptions: req.query })
+    } catch {
+        res.redirect('/')
+    }
 })
 
 // New Authors Route
